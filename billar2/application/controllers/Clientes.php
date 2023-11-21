29956c9e283f0a5aca9c2b4ab2b7e2edc6261458
Juan-CB <juancb198@gmail.com>
2023-11-20 21:01:00 -0500

    billar clientes

---
+++ billar2/application/controllers/Clientes.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Clientes extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('clientes_model');
+        $this->load->helper('url');
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+    }
+
+    public function index() {
+        $data['clientes'] = $this->clientes_model->obtener_clientes();
+        $this->load->view('admin/Clientes', $data);
+    }
+
+    public function buscar() {
+
+        if($_POST){
+            $busqueda = $this->input->post('busqueda');
+
+            $data['respuesta'] = $this->clientes_model->buscar_clientes($busqueda);
+            $this->load->view('admin/Clientes', $data);
+
+        } else {
+            redirect('Welcome');
+        }
+    }
+
+}
+?>
---
+++ billar2/application/controllers/Welcome.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	//Función para definir página de inicio
+	public function index()
+	{	
+		$this->load->view('welcome_message');
+	}
+}
