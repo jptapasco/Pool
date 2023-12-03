922643f51f426870755c7bc216c2dea07640048a
Juan-CB <juancb198@gmail.com>
2023-12-02 20:22:22 -0500

    eliminando documentos

--- Billar/application/views/Footer/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer text-center">
-
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; 2023-2024 <a href="https://adminlte.io">InovaByte</a>.</strong> Todos los derechos reservados.
-</footer>
