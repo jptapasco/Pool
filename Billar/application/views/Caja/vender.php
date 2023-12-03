f30c7243e6611a782e88d9286091df6af6e17f04
jptapasco <restrepofreddy28@gmail.com>
2023-12-03 14:11:18 -0500

    haga que no se pierdan los registros de la tabla

--- Billar/application/views/Caja/vender.php
+++ Billar/application/views/Caja/vender.php
@@ -21,39 +21,42 @@
     <!-- Site wrapper -->
     <div class="wrapper">
         <?php $this->load->view('Menu/menu'); ?>
+
         <h1 class="text-center mt-3">Venta de productos</h1>
         <div class="container">
             <?php echo form_open('productos/obtenerPorCategoria', 'id="categoriaForm"'); ?>
             <div class="row mt-4">
                 <div class="col-12">
                     <?php
-                        echo form_label('Elije la categoria del producto:', '');
+                    echo form_label('Elije la categoria del producto:', '');
 
-                        $options = array(
-                            ''                  => 'Categoria',                            
-                            'alcoholicas'       => 'Alcoholicas',
-                            'no_alcoholicas'    => 'No Alcoholicas',
-                            'comida'            => 'Comida',
-                            'snacks'            => 'Snacks',
-                        );
+                    $options = array(
+                        ''         => 'Categoria',
+                        'alcoholicas'    => 'Alcoholicas',
+                        'no_alcoholicas'  => 'No Alcoholicas',
+                        'comida'      => 'Comida',
+                        'snacks'      => 'Snacks',
+                    );
 
-                        $attributes = 'class="form-control input-lg border border-info"';
-                        echo form_dropdown('categoria', $options, '', $attributes);
+                    $attributes = 'class="form-control input-lg border border-info"';
+                    echo form_dropdown('categoria', $options, '', $attributes);
                     ?>
                 </div>
             </div>
             <?php echo form_close(); ?>
+
             <div class="row mt-3">
-                <?php if (!empty($respuesta_productos)): ?>
-                    <?php foreach ($respuesta_productos as $producto): ?>
-                        <div class="col-md-4 mb-3"> <!-- Utiliza la clase 'mb-3' para agregar margen inferior -->
+                <?php if (!empty($respuesta_productos)) : ?>
+                    <?php foreach ($respuesta_productos as $producto) : ?>
+                        <div class="col-md-4 mb-3">
                             <div class="card producto-card" data-nombre="<?php echo $producto->nombre; ?>" data-valor="<?php echo $producto->valor_venta; ?>" data-cantidad="<?php echo $producto->cantidad; ?>" style="width: 18rem;">
                                 <a href="#">
                                     <img src="https://www.cocinacaserayfacil.net/wp-content/uploads/2020/03/Platos-de-comida-que-pides-a-domicilio-y-puedes-hacer-en-casa.jpg" class="card-img-top" alt="...">
                                     <div class="card-body">
                                         <h5 class="card-text text-center"><?php echo $producto->nombre; ?></h5>
                                         <h5 class="card-text text-center"><?php echo $producto->valor_venta; ?></h5>
-                                        <h5 class="card-text text-center">Cantidad disponible: <?php echo $producto->cantidad; ?></h5>
+                                        <h5 class="card-text text-center">Cantidad disponible:
+                                            <?php echo $producto->cantidad; ?></h5>
                                     </div>
                                 </a>
                             </div>
@@ -61,6 +64,7 @@
                     <?php endforeach; ?>
                 <?php endif; ?>
             </div>
+
             <table class="table mt-4">
                 <thead>
                     <tr>
@@ -70,10 +74,9 @@
                         <th scope="col"></th>
                     </tr>
                 </thead>
-                <tbody id="tabla-ventas">
-
-                </tbody>
+                <tbody id="tabla-ventas"></tbody>
             </table>
+
             <!-- Botón para eliminar todos los elementos de la tabla -->
             <button id="btnEliminarTodo" class="btn btn-danger mt-3">Eliminar Todo</button>
         </div>
@@ -89,25 +92,39 @@
     <!-- /.control-sidebar -->
     </div>
     <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url() ?>/assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?php echo base_url() ?>/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url() ?>/assets/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?php echo base_url() ?>/assets/dist/js/demo.js"></script>
+    <!-- Buscar -->
+    <script src="<?php echo base_url() ?>/assets/dist/js/buscar.js"></script>
+
     <script>
-        document.getElementById('categoriaForm').addEventListener('change', function () {
+        document.getElementById('categoriaForm').addEventListener('change', function() {
             var form = document.getElementById('categoriaForm');
             if (form) {
                 form.submit();
             }
         });
-    </script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            // Recuperar la información almacenada en el almacenamiento local al cargar la página
-            var ventasGuardadas = JSON.parse(localStorage.getItem('ventas')) || [];
-            var tablaVentas = document.getElementById('tabla-ventas');
-            let cantidad_llevar = parseInt("1", 10);
 
-            // Función para eliminar una fila al hacer clic en el botón "Cancelar"
+        document.addEventListener('DOMContentLoaded', function() {
+            var cantidad_llevar = parseInt("1", 10);
+            var tablaVentas = document.getElementById('tabla-ventas');
+
+            function obtenerVentasGuardadasPorCategoria(categoria) {
+                var ventasGuardadas = JSON.parse(localStorage.getItem('ventas')) || [];
+                return ventasGuardadas.filter(function(venta) {
+                    return venta.categoria === categoria;
+                });
+            }
+
             function eliminarFila(fila, nombre) {
                 fila.remove();
-                // Actualizar la información en el almacenamiento local después de eliminar la fila
                 var ventasActualizadas = JSON.parse(localStorage.getItem('ventas')) || [];
                 var nuevaListaVentas = ventasActualizadas.filter(function(venta) {
                     return venta.nombre !== nombre;
@@ -115,7 +132,6 @@
                 localStorage.setItem('ventas', JSON.stringify(nuevaListaVentas));
             }
 
-            // Función para eliminar todas las filas y reiniciar el almacenamiento local
             function cancelarTodo() {
                 while (tablaVentas.firstChild) {
                     tablaVentas.removeChild(tablaVentas.firstChild);
@@ -123,23 +139,43 @@
                 localStorage.removeItem('ventas');
             }
 
-            // Función para agregar un producto a la tabla
-            function agregarProducto(nombre, valor, cantidad) {
-                // Verificar si el producto ya está en la tabla
+            // Almacenar la información actualizada en el almacenamiento local
+            function actualizarLocalStorage() {
+                var ventasActualizadas = JSON.parse(localStorage.getItem('ventas')) || [];
+
+                // Verificar si el producto ya está en el almacenamiento local
+                var ventaExistente = ventasActualizadas.find(function(venta) {
+                    return venta.nombre === nombre && venta.categoria === categoria;
+                });
+
+                if (ventaExistente) {
+                    // Si el producto ya está, actualizar la cantidad
+                    ventaExistente.cantidad += cantidad;
+                } else {
+                    // Si el producto no está, agregarlo al almacenamiento local
+                    ventasActualizadas.push({
+                        nombre: nombre,
+                        valor: valor,
+                        cantidad: cantidad,
+                        categoria: categoria // Agregar la categoría al objeto
+                    });
+                }
+
+                localStorage.setItem('ventas', JSON.stringify(ventasActualizadas));
+            }
+
+            function agregarProducto(nombre, valor, cantidad, categoria) {
                 var filaExistente = Array.from(tablaVentas.children).find(function(fila) {
-                    return fila.querySelector('td:first-child').innerText === nombre;
+                    return fila.querySelector('td:first-child').innerText === nombre && fila.dataset.categoria === categoria;
                 });
 
                 if (filaExistente) {
-                    // Si el producto ya está en la tabla, actualizar la cantidad
                     cantidad = parseInt(filaExistente.querySelector('td:last-child').innerText, 10) + cantidad;
                     filaExistente.querySelector('td:last-child').innerText = cantidad;
-                    // Actualizar la información en el almacenamiento local
                     actualizarLocalStorage();
                     return;
                 }
 
-                // Botón "Cancelar" en cada fila de la tabla de ventas
                 var btnCancelar = document.createElement('button');
                 btnCancelar.innerText = 'Cancelar';
                 btnCancelar.classList.add('btn', 'btn-danger', 'btn-sm');
@@ -147,7 +183,6 @@
                     eliminarFila(fila, nombre);
                 });
 
-                // Botones de incremento y decremento
                 var btnIncrementar = document.createElement('button');
                 btnIncrementar.innerText = '+';
                 btnIncrementar.classList.add('btn', 'btn-success', 'btn-sm', 'mx-1');
@@ -166,32 +201,19 @@
                     }
                 });
 
-                // Función para actualizar la cantidad en la fila
                 function actualizarCantidad() {
                     cantidadCell.innerText = cantidad;
-                    // Actualizar la información en el almacenamiento local
                     actualizarLocalStorage();
                 }
 
-                // Añadir los botones de incremento y decremento a la fila
                 var fila = document.createElement('tr');
+                fila.dataset.categoria = categoria; // Asignar la categoría a la fila
                 fila.innerHTML = '<td>' + nombre + '</td><td>' + valor + '</td><td>' + cantidad + '</td>';
                 var cantidadCell = fila.querySelector('td:last-child');
                 fila.appendChild(document.createElement('td').appendChild(btnIncrementar));
                 fila.appendChild(document.createElement('td').appendChild(btnDecrementar));
                 fila.appendChild(document.createElement('td').appendChild(btnCancelar));
                 tablaVentas.appendChild(fila);
-
-                // Almacenar la información actualizada en el almacenamiento local
-                function actualizarLocalStorage() {
-                    var ventasActualizadas = JSON.parse(localStorage.getItem('ventas')) || [];
-                    ventasActualizadas.push({
-                        nombre: nombre,
-                        valor: valor,
-                        cantidad: cantidad
-                    });
-                    localStorage.setItem('ventas', JSON.stringify(ventasActualizadas));
-                }
 
                 actualizarLocalStorage();
             }
@@ -203,37 +225,35 @@
                     var nombre = tarjeta.getAttribute('data-nombre');
                     var valor = tarjeta.getAttribute('data-valor');
                     var cantidad = cantidad_llevar;
+                    var categoria = document.querySelector('select[name="categoria"]').value;
 
-                    // Llamar a la función para agregar el producto
-                    agregarProducto(nombre, valor, cantidad);
+                    agregarProducto(nombre, valor, cantidad, categoria);
                 });
             });
 
-            // Agregar las ventas guardadas a la tabla al cargar la página
-            ventasGuardadas.forEach(function(venta) {
-                agregarProducto(venta.nombre, venta.valor, cantidad_llevar);
+            // Actualizar la tabla al cambiar de categoría
+            document.getElementById('categoriaForm').addEventListener('change', function() {
+                var categoriaSeleccionada = document.querySelector('select[name="categoria"]').value;
+                var ventasGuardadasPorCategoria = obtenerVentasGuardadasPorCategoria(categoriaSeleccionada);
+
+                // Limpiar la tabla actual
+                while (tablaVentas.firstChild) {
+                    tablaVentas.removeChild(tablaVentas.firstChild);
+                }
+
+                // Agregar las ventas guardadas de la categoría actual a la tabla
+                ventasGuardadasPorCategoria.forEach(function(venta) {
+                    agregarProducto(venta.nombre, venta.valor, venta.cantidad, categoriaSeleccionada);
+                });
             });
 
-            // Evento para el botón "Cancelar Todo"
-            var btnCancelarTodo = document.getElementById('btnCancelarTodo');
-            btnCancelarTodo.addEventListener('click', function() {
+            var btnEliminarTodo = document.getElementById('btnEliminarTodo');
+
+            btnEliminarTodo.addEventListener('click', function() {
                 cancelarTodo();
             });
         });
     </script>
-
-
-
-
-    <!-- jQuery -->
-    <script src="<?php echo base_url() ?>/assets/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="<?php echo base_url() ?>/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="<?php echo base_url() ?>/assets/dist/js/adminlte.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="<?php echo base_url() ?>/assets/dist/js/demo.js"></script>
-    <!-- Buscar -->
-    <script src="<?php echo base_url() ?>/assets/dist/js/buscar.js"></script>
 </body>
+
 </html>
